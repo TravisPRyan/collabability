833e43b9ea3e26030c61f58549c3087f97fe1616
TravisPRyan <TravisPRyan@protonmail.com>
2020-01-23 16:18:58 -0500

    v13 Developer

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Push 12: Implement search feature.
+Push 13: Implement dev reducer, action, and component.
+
+Collabability - Accountability logs for remote teams. Created in React with Redux for state management and JSON Server as a mock back-end.
+
+Usage:<br/>
+<br/>
+npm install<br/>
+npm run dev<br/>
+Runs the app with the backend JSON-Server<br/>
+Open http://localhost:3000 Server http://localhost:5000 db.json contains data<br/>
+<br/>
+npm run build<br/>
+Builds the app for production to the build folder.
--- src/components/devs/DevListModal.js
+++ src/components/devs/DevListModal.js
@@ -1,34 +1,36 @@
 import React, { useState, useEffect } from "react";
 import DevItem from "./DevItem";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getDevs } from "../../actions/devActions";
 
-const DevListModal = () => {
-  const [devs, setDevs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const DevListModal = ({ getDevs, dev: { devs, loading } }) => {
   useEffect(() => {
     getDevs();
-    //eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
-
-  const getDevs = async () => {
-    setLoading(true);
-    const res = await fetch("/devs");
-    const data = await res.json();
-
-    setDevs(data);
-    setLoading(false);
-  };
 
   return (
     <div id="dev-list-modal" className="modal">
       <div className="modal-content">
         <h4>DeveloperList</h4>
         <ul className="collection">
-          {!loading && devs.map(dev => <DevItem dev={dev} key={dev.id} />)}
+          {!loading &&
+            devs !== null &&
+            devs.map(dev => <DevItem dev={dev} key={dev.id} />)}
         </ul>
       </div>
     </div>
   );
 };
 
-export default DevListModal;
+DevListModal.propTypes = {
+  dev: PropTypes.object.isRequired,
+  getDevs: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  dev: state.dev
+});
+
+export default connect(mapStateToProps, { getDevs })(DevListModal);
