60b58094b90a62229c2e4995db447ef161a9d877
TravisPRyan <TravisPRyan@protonmail.com>
2020-01-29 16:40:50 -0500

    v15 Delete

--- src/components/devs/DevItem.js
+++ src/components/devs/DevItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { deleteDev } from "../../actions/devActions";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-const DevItem = ({ dev }) => {
+const DevItem = ({ dev, deleteDev }) => {
+  const onDelete = () => {
+    deleteDev(dev.id);
+    M.toast({ html: "Developer removed from list." });
+  };
+
   return (
     <li className="collection-item">
       <div>
         {dev.firstName} {dev.lastName}
-        <a href="#!" className="secondary-content">
+        <a href="#!" className="secondary-content" onClick={onDelete}>
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
@@ -15,7 +23,8 @@ const DevItem = ({ dev }) => {
 };
 
 DevItem.propTypes = {
-  dev: PropTypes.object.isRequired
+  dev: PropTypes.object.isRequired,
+  deleteDev: PropTypes.func.isRequired
 };
 
-export default DevItem;
+export default connect(null, { deleteDev })(DevItem);
